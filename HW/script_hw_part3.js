b0f08e25e3039ed954d7876ddf99840a46b136eb
Dmytro <dmytro.chushko@gmail.com>
2021-05-14 01:27:29 +0300

    HW_part_3

--- HW/script_hw_part3.js
+++ HW/script_hw_part3.js
@@ -36,18 +36,6 @@ let personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
-
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
         const a = prompt('Один из последних просмотренных фильмов?', ''),
@@ -64,15 +52,30 @@ function rememberMyFilms() {
 
 rememberMyFilms();
 
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');    
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Вы ввели недопустимое значение');
+    }
+}
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');    
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    alert('Вы киноман');
-} else {
-    alert('Вы ввели недопустимое значение');
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером - ${i}`);
+    }
 }
 
 // let nameOfMovie = prompt('Один из последних просмотренных фильмов?', '');
